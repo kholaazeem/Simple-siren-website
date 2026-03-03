215c725a5a7332d2a6e0e5f7bc799e5e80cce511
khola azeem <kholaazeem104@gmail.com>
2026-03-04 00:20:21 +0500

    hhjh

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+// import React , {useState} from "react";
+
+
+
+// const Navbar = () => {
+//    const [activeItem , setActiveItem] = useState('Home');
+//    const navItems = ['Home' , 'About' , 'Shop', 'Blog']
+//   return (
+//    <div className="fixed-top d-flex justify-content-center mt-4 px-3" style={{zIndex: 1050}}>
+//       {/* Navbar card */}
+//     <nav className="bg-white rounded-pill d-flex allign-item-center justify-content-between px-4 py-2" style={{width: '100%', maxWidth: '750px', boxShadow: '0 8px 30px rgba(0,0,0,0.06'}}>
+     
+//      {/* Logo section */}
+     
+//     </nav>
+//    </div>
+//   )
+// }
+
+// export default Navbar
